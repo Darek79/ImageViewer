af4e1045b57e9b70578274168a4c1529f62d76cb
Darek K <darius.kloda@gmail.com>
2019-05-12 21:07:06 +0200

    added fullscreenview
    added fullscreenview

--- src/public/js/pageEffects.js
+++ src/public/js/pageEffects.js
@@ -4,20 +4,52 @@
 const img = document.querySelectorAll(".img");
 const arr = Array.from(img);
 const l = arr.length;
+const wr = document.getElementById("fullWrapper");
+const imgDis = document.getElementById("fullView");
 
 console.log(l);
 let x = 0;
-const shwoIMG = y => {
+
+const showIMG = y => {
 	if (y === 0) {
 		return;
 	}
 	setTimeout(() => {
 		arr[x++].hidden = false;
-		shwoIMG(y - 1);
-	}, 500);
+		showIMG(y - 1);
+		if (x === l) {
+			fullV();
+		}
+	}, 100);
 };
-shwoIMG(l);
-// img.addEventListener("click", e => {
-// 	console.log(e.target);
-// });
-// console.log(arr);
+showIMG(l);
+// (async () => {
+// 	const x = await showIMG();
+// 	console.log(x);
+// })();
+
+const fullV = async () => {
+	console.log("done");
+	let el = new Promise((res, rej) => {
+		arr.forEach((val, i) => {
+			val.addEventListener("click", e => {
+				wr.style.display = "flex";
+				e.target.classList.remove("img");
+				e.target.classList.add("added");
+				imgDis.appendChild(e.target);
+				console.log("clicked");
+				res(e.target);
+			});
+		});
+	});
+	// el.then(el => {
+	// 	el.addEventListener(
+	// 		("click",
+	// 		() => {
+	// 			wr.style.display = "none";
+	// 			e.target.classList.add("img");
+	// 			e.target.classList.remove("added");
+	// 		})
+	// 	);
+	// });
+};
