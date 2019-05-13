eb4c71d0261944f8df12dd276bed09b373f730be
Darek K <darius.kloda@gmail.com>
2019-05-13 12:40:07 +0200

    finished image Viewer
    finished image Viewer

--- src/public/js/pageEffects.js
+++ src/public/js/pageEffects.js
@@ -11,45 +11,67 @@ console.log(l);
 let x = 0;
 
 const showIMG = y => {
-	if (y === 0) {
-		return;
-	}
-	setTimeout(() => {
-		arr[x++].hidden = false;
-		showIMG(y - 1);
-		if (x === l) {
-			fullV();
-		}
-	}, 100);
+  if (y === 0) {
+    return;
+  }
+  setTimeout(() => {
+    arr[x++].hidden = false;
+    showIMG(y - 1);
+    if (x === l) {
+      fullV();
+    }
+  }, 100);
 };
 showIMG(l);
-// (async () => {
-// 	const x = await showIMG();
-// 	console.log(x);
-// })();
-
 const fullV = async () => {
-	console.log("done");
-	let el = new Promise((res, rej) => {
-		arr.forEach((val, i) => {
-			val.addEventListener("click", e => {
-				wr.style.display = "flex";
-				e.target.classList.remove("img");
-				e.target.classList.add("added");
-				imgDis.appendChild(e.target);
-				console.log("clicked");
-				res(e.target);
-			});
-		});
-	});
-	// el.then(el => {
-	// 	el.addEventListener(
-	// 		("click",
-	// 		() => {
-	// 			wr.style.display = "none";
-	// 			e.target.classList.add("img");
-	// 			e.target.classList.remove("added");
-	// 		})
-	// 	);
-	// });
+  console.log("done");
+  let el = new Promise((res, rej) => {
+    arr.forEach((val, i) => {
+      val.addEventListener("click", e => {
+        wr.style.display = "flex";
+        e.target.classList.remove("img");
+        e.target.classList.add("added");
+        imgDis.appendChild(e.target);
+        console.log("click");
+        res(e.target);
+      });
+    });
+  });
+  el.then(el => {
+    closeIMG(el);
+  });
 };
+
+const closeIMG = e => {
+  console.log(e);
+  const p = document.createElement("p");
+  const x = document.createTextNode("x");
+  p.appendChild(x);
+  p.style = `
+	display: absolute;
+	top: 0;
+	font-size:50px;
+	cursor: pointer;
+	`;
+  wr.appendChild(p);
+  p.id = "close";
+  const pClose = document.getElementById("close");
+  pClose.addEventListener("click", () => {
+    wr.style.display = "none";
+  });
+};
+
+// el.then(el => {
+// 	el.addEventListener(
+// 		("click",
+// 		() => {
+// 			wr.style.display = "none";
+// 			e.target.classList.add("img");
+// 			e.target.classList.remove("added");
+// 		})
+// 	);
+// });
+
+// const loopIMG = arr => {
+
+// };
